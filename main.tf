079408bc66bdeab6f388ec6c13495d1b3d18e2f1
memetics19 <shreeda.bhat@yahoo.com>
2021-05-19 11:44:48 +0530

    added ec2 instances

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "main" {
 data "aws_availability_zones" "main_availbility_zone" {
   state = "available"
 }
-resource "aws_subnet" "public-1" {
+resource "aws_subnet" "public_1" {
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.0.1.0/24"
   availability_zone = data.aws_availability_zones.main_availbility_zone.names[0]
@@ -23,7 +23,7 @@ resource "aws_subnet" "public-1" {
   }
 }
 
-resource "aws_subnet" "public-2" {
+resource "aws_subnet" "public_2" {
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.0.2.0/24"
   availability_zone = data.aws_availability_zones.main_availbility_zone.names[1]
@@ -32,7 +32,7 @@ resource "aws_subnet" "public-2" {
   }
 }
 
-resource "aws_subnet" "private-1" {
+resource "aws_subnet" "private_1" {
   vpc_id                  = "${aws_vpc.main.id}"
   cidr_block              = "10.0.3.0/24"
   availability_zone       = data.aws_availability_zones.main_availbility_zone.names[1]
@@ -42,7 +42,7 @@ resource "aws_subnet" "private-1" {
 
   }
 }
-resource "aws_subnet" "private-2" {
+resource "aws_subnet" "private_2" {
   vpc_id                  = "${aws_vpc.main.id}"
   cidr_block              = "10.0.4.0/24"
   availability_zone       = data.aws_availability_zones.main_availbility_zone.names[1]
@@ -53,7 +53,44 @@ resource "aws_subnet" "private-2" {
   }
 }
 
+resource "aws_security_group" "ssh" {
+    vpc_id = "${aws_vpc.main.id}"
+    
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["10.0.1.0/24"]
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
 
+        cidr_blocks = ["10.0.1.0/24"]
+    } 
+}
 
+variable "AMI" {    
+    default = {
+        eu-west-2 = "ami-03dea29b0216a1e03"
+        us-east-1 = "ami-0c2a1acae6667e438"
+    }
+}
+variable "AWS_REGION" {    
+    default = "eu-west-2"
+}
 
+resource "aws_instance" "ec2_1" {
+    ami = "${lookup(var.AMI, var.AWS_REGION)}"
+    instance_type = "t2.micro"
+    subnet_id = "${aws_subnet.public_1.id}"
+    vpc_security_group_ids = ["${aws_security_group.ssh.id}"]
+}
+
+resource "aws_instace" "ec2_2" {
+    ami = "${lookup(var.AMI, var.AWS_REGION)}"
+    instance_type  = "t2.micro"
+    subnet_id = "${aws_subnet.private_1.id}"
+}
 
