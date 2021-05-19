c750273d366bdec2ede8de5636e3543011ce25c5
memetics19 <shreeda.bhat@yahoo.com>
2021-05-19 11:27:49 +0530

    added cidr block

--- main.tf
+++ main.tf
@@ -22,17 +22,38 @@ resource "aws_subnet" "public-1" {
     "Name" = "public-1"
   }
 }
-resource "aws_internet_gateway" "public-gateway" {
-    vpc_id = "${aws_vpc.main.id}"
-    tags {
-        Name = "public-gateway"
-    }
-}
 
 resource "aws_subnet" "public-2" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "10.0.2.0/24"
   availability_zone = data.aws_availability_zones.main_availbility_zone.names[1]
-
-
+  tags = {
+    "Name" = "public-2"
+  }
 }
+
+resource "aws_subnet" "private-1" {
+  vpc_id                  = "${aws_vpc.main.id}"
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = data.aws_availability_zones.main_availbility_zone.names[1]
+  map_public_ip_on_launch = false
+  tags = {
+    Name        = "private-1"
+
+  }
+}
+resource "aws_subnet" "private-2" {
+  vpc_id                  = "${aws_vpc.main.id}"
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = data.aws_availability_zones.main_availbility_zone.names[1]
+  map_public_ip_on_launch = false
+  tags = {
+    Name        = "private-2"
+
+  }
+}
+
+
+
 
 
